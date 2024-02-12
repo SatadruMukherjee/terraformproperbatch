a2eba20c14b280a53a4a4341d5e5fdf4539cedfb
SatadruMukherjee Madhusudan <espl18142@EPILAP907.local>
2024-02-13 01:05:07 +0530

    Daily Changes

--- terraform_code/provider.tf
+++ terraform_code/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-  backend "s3" {
-    bucket = "demoyttesttrail"
-    key    = "test123/terraform/terraform.tfstate"
-    region     = var.AWS_DEFAULT_REGION
-  }
-  required_version = "~> 1.0"
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+#   backend "s3" {
+#     bucket = "demoyttesttrail"
+#     key    = "test123/terraform/terraform.tfstate"
+#     region     = var.AWS_DEFAULT_REGION
+#   }
+#   required_version = "~> 1.0"
+# }
 
 
 provider "aws" {
